a84a14f9e9aefead91d4dfaebd7fcf4e8a7e6ff5
Josefina Urquiza <josefina.urquizap@gmail.com>
2026-06-30 16:32:47 -0300

    check de dependencias

--- scripts/verificacion_paquete.R
+++ scripts/verificacion_paquete.R
@@ -1,18 +1,18 @@
-# 03 - Verificación del paquete
+#Verificación del paquete
 #
 # Objetivo: Ejecutar chequeos automáticos para validar estructura y errores
 
-# Ejecutar R CMD check del paquete
+# Ejecutar R CMD check del paquete para ver si falta algo
+# Por ejemplo setear la licencia
 devtools::check()
 
-# Cargar paquete en modo desarrollo
-devtools::load_all()
-
-####
-# 04 - Licencia del paquete
+# Licencia del paquete
 #
 # Objetivo: Definir licencia del proyecto (MIT en este caso)
 ###############################################################
 
 # Agregar licencia MIT al paquete
 usethis::use_mit_license()
+
+# Cargar paquete en modo  para actualizar lo de la licencia
+devtools::load_all()
