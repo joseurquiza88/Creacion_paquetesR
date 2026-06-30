8a990658b15f3ab8e542da8cbff2e483de01ee32
Josefina Urquiza <josefina.urquizap@gmail.com>
2026-06-30 16:24:27 -0300

    configuracion git-github

--- scripts/configuracion_git_github.R
+++ scripts/configuracion_git_github.R
@@ -1,10 +1,11 @@
 # 01 - Configuracion de Git y GitHub
 #
-# Objetivo: ----
-# Autenticarse en GitHub y verificar conexión con Git local
-###############################################################
+# Objetivo: Autenticarse en GitHub y verificar conexión con Git local
+
 
 # Crear token de acceso personal en GitHub
+# Esto hay que congigurarlo en el perfild e github personal
+#Settings ==> Developer Settings ==> Personal access tokens (classic)
 usethis::create_github_token()
 
 # Guardar credenciales en el sistema local
@@ -15,3 +16,5 @@ usethis::git_sitrep()
 
 # Verificar repos remotos configurados
 system("git remote -v")
+
+# Verificar los datos del usuario del R .Rprofile
